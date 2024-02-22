33759bfa39926470047d8ac10e8b48844b90f7ad
kaiser7410 <kaiser7410@hotmail.com>
2024-02-22 16:44:51 -0500

    paginacion y busqueda

--- src/main/java/com/jcg/gestioncursos/controller/CursoController.java
+++ src/main/java/com/jcg/gestioncursos/controller/CursoController.java
@@ -6,16 +6,22 @@ import com.jcg.gestioncursos.reports.CursoExporterPDF;
 import com.jcg.gestioncursos.repository.CursoRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -31,7 +37,32 @@ public class CursoController {
     }
 
     @GetMapping("/cursos")
-    public String listarCursos(Model model){
+    public String listarCursos(Model model, @Param("keyword") String keyword, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "3") int size){
+        try {
+            List<Curso> cursos = new ArrayList<>();
+            Pageable paging = PageRequest.of(page-1,size);
+
+            Page<Curso> pageCursos = null;
+
+            if(keyword ==null){
+                pageCursos = cursoRepository.findAll(paging);
+            }
+            else{
+                pageCursos = cursoRepository.findByTituloContainingIgnoreCase(keyword,paging);
+                model.addAttribute("keyword",keyword);
+            }
+
+            cursos = pageCursos.getContent();
+
+            model.addAttribute("cursos",cursos);
+            model.addAttribute("currentPage",pageCursos.getNumber() + 1);
+            model.addAttribute("totalItems",pageCursos.getTotalElements());
+            model.addAttribute("totalPages",pageCursos.getTotalPages());
+            model.addAttribute("pageSize",size);
+        }catch (Exception exception){
+            model.addAttribute("message",exception.getMessage());
+        }
+
         List<Curso> cursos = cursoRepository.findAll();
         cursos = cursoRepository.findAll();
         model.addAttribute("cursos",cursos);
--- src/main/java/com/jcg/gestioncursos/repository/CursoRepository.java
+++ src/main/java/com/jcg/gestioncursos/repository/CursoRepository.java
@@ -1,9 +1,12 @@
 package com.jcg.gestioncursos.repository;
 
 import com.jcg.gestioncursos.entity.Curso;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CursoRepository extends JpaRepository<Curso,Integer> {
+    Page<Curso> findByTituloContainingIgnoreCase (String keyword, Pageable pageable);
 }
